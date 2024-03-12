ce4f7f0240bb8274309dcbfd220f281f739eaed2
alternativefuture <yepremyanhovhannes11@gmail.com>
2024-03-12 16:58:24 +0400

    feat: implement threading

--- web_soket/socket_client.py
+++ web_soket/socket_client.py
@@ -1,4 +1,21 @@
 import socket
+import threading as thread
+
+
+def send_message():
+    while True:
+        message = input("You: ")
+        client_socket.send(message.encode())
+
+
+def res_message():
+    while True:
+        try:
+            received_message = client_socket.recv(1024).decode()
+            print(f"Client: {received_message}")
+        except TimeoutError:
+            continue
+
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = '0.0.0.0'
@@ -8,19 +25,15 @@ client_socket.connect((host, port))
 print(f"Connected to {host}:{port}")
 client_socket.settimeout(0.2)
 
-while True:
-    try:
-        received_message = client_socket.recv(1024).decode()
-        print(f"Server: {received_message}")
-        if received_message.lower() == 'bye':
-            break
-    except TimeoutError:
-        continue
 
-    message = input("You: ")
-    client_socket.send(message.encode())
-    if message.lower() == 'bye':
-        break
+t1 = thread.Thread(target=send_message)
+
+t2 = thread.Thread(target=res_message)
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
 
 client_socket.close()
-
--- web_soket/socket_server.py
+++ web_soket/socket_server.py
@@ -1,33 +1,47 @@
 import socket
+import threading as thread
+
+
+def send_message():
+    while True:
+        message = input("You: ")
+        client_socket.send(message.encode())
+
+
+def res_message():
+    while True:
+        try:
+            received_message = client_socket.recv(1024).decode()
+            print(f"Client: {received_message}")
+        except TimeoutError:
+            continue
+
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = '0.0.0.0'
-port = 5000
 
+port = 5000
 server_socket.bind((host, port))
+
 server_socket.listen()
 
 print(f"Server listening on {host}:{port}")
-
 client_socket, client_address = server_socket.accept()
 print(f"Connection from {client_address}")
+
+
 client_socket.settimeout(0.2)
 
-while True:
-    message = input("You: ")
-    client_socket.send(message.encode())
-    if message.lower() == 'bye':
-        break
 
-    try:
-        received_message = client_socket.recv(1024).decode()
-        print(f"Client: {received_message}")
-        if received_message.lower() == 'bye':
-            break
-    except TimeoutError:
-        continue
+t1 = thread.Thread(target=send_message)
 
+t2 = thread.Thread(target=res_message)
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
 
 client_socket.close()
 server_socket.close()
-
